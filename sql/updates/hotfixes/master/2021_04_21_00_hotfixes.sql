2916135877741a5a935e4c731bdd1397e199a896
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-05-28 16:04:04 +0300

    restore

--- sql/updates/hotfixes/master/2021_04_21_00_hotfixes.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM hotfix_blob WHERE Tablehash IN (0xDF5FA28);
-DELETE FROM hotfix_data WHERE Tablehash IN (0xEFB13552);
--- sql/updates/world/master/2021_05_21_06_world.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `creature_template` SET subname = replace(subname,'Â','');
