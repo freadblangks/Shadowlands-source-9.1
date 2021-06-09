8fbd43e2748b78f99889f6761a0e7241a7c41cf4
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-10 00:01:05 +0300

    Update boss_surgeon_stitchflesh.cpp

--- src/server/scripts/Shadowlands/Bastion/NecroticWake/boss_surgeon_stitchflesh.cpp
+++ src/server/scripts/Shadowlands/Bastion/NecroticWake/boss_surgeon_stitchflesh.cpp
@@ -1,4 +1,16 @@
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "CreatureAI.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "SpellAuraEffects.h"
+#include "Map.h"
 #include "necrotic_wake.h"
+#include "npc_Bastion"
+#include "objects_Bastion"
 
 void AddSC_boss_surgeon_stitchflesh()
 {
