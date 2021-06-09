ff681ded863c2d73efca3ab872bb209e3fff5151
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-10 00:00:08 +0300

    Update boss_nalthor.cpp

--- src/server/scripts/Shadowlands/Bastion/NecroticWake/boss_nalthor.cpp
+++ src/server/scripts/Shadowlands/Bastion/NecroticWake/boss_nalthor.cpp
@@ -1,6 +1,16 @@
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "CreatureAI.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "SpellAuraEffects.h"
+#include "Map.h"
+#include "necrotic_wake.h"
 #include "npc_Bastion"
 #include "objects_Bastion"
-#include "necrotic_wake.h"
 
 void AddSC_boss_nalthor()
 {
